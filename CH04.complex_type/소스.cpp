36289d81fa0a86dda1c09a1b4eae0db8a5bfa848
Parkseojin2001 <seojin10912@naver.com>
2022-05-04 21:50:43 +0900

    Update addpntrs.cpp

--- CH04.complex_type/소스.cpp
+++ CH04.complex_type/소스.cpp
@@ -1,4 +1,24 @@
-//arraynew.cpp
+/*use_new.cpp
+#include <iostream>
+int main() {
+	using namespace std;
+	int nights = 1001;
+	int* pt = new int;
+	*pt = 1001;
+	cout << "night value = ";
+	cout << nights << ": memory location " << &nights << endl;
+	cout << "int type ";
+	cout << "value = " << *pt << ": memory location = " << pt << endl;
+	double* pd = new double;
+	*pd = 1000001.0;
+	cout << "double type";
+	cout << "value = " << *pd << ":memory location = " << pd << endl;
+	cout << "pt size = " << sizeof(*pt) << endl;
+	cout << "pd size = " << sizeof pd;
+	cout << ": *pd size = " << sizeof(*pd) << endl;
+	return 0;
+}*/
+/*arraynew.cpp
 #include <iostream>
 int main() {
 	using namespace std;
@@ -11,5 +31,32 @@ int main() {
 	cout << "Now, p3[0] is " << p3[0] << endl;
 	p3 = p3 - 1;
 	delete[]p3;
+	return 0;
+}*/
+//addpntrs.cpp
+#include <iostream>
+int main() {
+	using namespace std;
+	double wages[3] = { 100.0,200.0,3000.0 };
+	short stacks[3] = { 3,2,1 };
+	double* pw = wages;
+	short* ps = &stacks[0];
+	cout << "pw = " << pw << ", *pw = " << *pw << endl;
+	pw = pw + 1;
+	cout << "pw pointer plus 1:\n";
+	cout << "pw = " << pw << ", *pw = " << *pw << "\n\n";
+	
+	cout << "ps = " << ps << ", *ps = " << *ps << "\n\n";
+	ps = ps + 1;
+	cout << "ps pointer plus 1:\n";
+	cout << "ps = " << ps << ", *ps =" << *ps << "\n\n";
+
+	cout << "Access as an array\n";
+	cout << "stacks[0] = " << stacks[0] << ", stacks[1] = " << stacks[1] << endl;
+	cout << "access as a pointer\n";
+	cout << "*stacks = " << *stacks << ", *(stacks+1) = " << *(stacks + 1) << endl;
+
+	cout << sizeof(wages) << " = wages array size\n";
+	cout << sizeof(pw) << " = pw pointer size\n";
 	return 0;
 }
