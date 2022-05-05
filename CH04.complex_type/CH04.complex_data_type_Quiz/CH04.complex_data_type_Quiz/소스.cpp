23efabfa567d347ee1a79b02cac70c7ead08d728
Parkseojin2001 <seojin10912@naver.com>
2022-05-05 21:40:07 +0900

    Update Quiz3 answer

--- CH04.complex_type/CH04.complex_data_type_Quiz/CH04.complex_data_type_Quiz/소스.cpp
+++ CH04.complex_type/CH04.complex_data_type_Quiz/CH04.complex_data_type_Quiz/소스.cpp
@@ -1,15 +1,17 @@
 #pragma warning(disable:4996)
 #include <iostream>
 #include <cstring>
-#include <string>
-//Quiz2
+//Quiz3
 int main() {
 	using namespace std;
-	string name, dessert;
-	cout << "Enter your name: \n";
-	getline(cin, name);
-	cout << "Enter your favorite dessert: \n";
-	getline(cin, dessert);
-	cout << "I will prepare a delicious " << dessert << ". Mr." << name << "!";
+	char first[20],last[20],full[40];
+	cout << "Enter your first name: ";
+	cin.getline(first, 20);
+	cout << "Enter your last name: ";
+	cin.getline(last, 20);
+	strcpy(full, last);
+	strcat(full, ", ");
+	strcat(full, first);
+	cout << "If you make it into a single string: " << full << endl;
 	return 0;
 }
