72c110155bef18c75a8482e7f353aa42f52271b1
Parkseojin2001 <seojin10912@naver.com>
2022-05-05 21:33:26 +0900

    Update Quiz2 answer

--- CH04.complex_type/CH04.complex_data_type_Quiz/CH04.complex_data_type_Quiz/소스.cpp
+++ CH04.complex_type/CH04.complex_data_type_Quiz/CH04.complex_data_type_Quiz/소스.cpp
@@ -2,22 +2,14 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+//Quiz2
 int main() {
 	using namespace std;
-	char grade;
-	int age;
-	string first, last;
-	cout << "First Name: ";
-	getline(cin, first);
-	cout << "Last Name: ";
-	getline(cin, last);
-	cout << "The grade student wants: ";
-	cin >> grade;
-	cout << "Age: ";
-	cin >> age;
-	grade += 1;
-	cout << "Name: " << last << ", " << first << endl;
-	cout << "Grade: " << grade << endl;
-	cout << "Age: " << endl;
+	string name, dessert;
+	cout << "Enter your name: \n";
+	getline(cin, name);
+	cout << "Enter your favorite dessert: \n";
+	getline(cin, dessert);
+	cout << "I will prepare a delicious " << dessert << ". Mr." << name << "!";
 	return 0;
 }
