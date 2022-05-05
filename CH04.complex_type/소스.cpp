6d36b9253e61309d57031b11b03767467c0ebe56
Parkseojin2001 <seojin10912@naver.com>
2022-05-05 18:27:54 +0900

    Update mitypes.cpp

--- CH04.complex_type/소스.cpp
+++ CH04.complex_type/소스.cpp
@@ -1,24 +1,22 @@
 #pragma warning(disable:4996)
-//delete.cpp
+//mixtypes.cpp
 #include <iostream>
-#include <cstring>
-using namespace std;
-char* getname(void);
+struct antarctica_years_end {
+	int year;
+};
 int main() {
-	char* name;
-	name = getname();
-	cout << (int*)name << ": " << name << "\n";
-	delete[]name;
-	name = getname();
-	cout << (int*)name << ": " << name << "\n";
-	delete[]name;
+	antarctica_years_end s01, s02, s03;
+	s01.year = 1998;
+	antarctica_years_end* pa = &s02;
+	pa->year = 1999;
+	antarctica_years_end trio[3];
+	trio[0].year = 2003;
+	std::cout << trio->year << std::endl;
+	const antarctica_years_end* arp[3] = { &s01,&s02,&s03 };
+	std::cout << arp[1]->year << std::endl;
+	const antarctica_years_end** ppa = arp;
+	auto ppb = arp;
+	std::cout << (*ppa)->year << std::endl;
+	std::cout << (*(ppb + 1))->year << std::endl;
 	return 0;
-}
-char* getname() {
-	char temp[80];
-	cout << "Enter your name: ";
-	cin >> temp;
-	char* pn = new char[strlen(temp) + 1];
-	strcpy(pn, temp);
-	return pn;
 }
