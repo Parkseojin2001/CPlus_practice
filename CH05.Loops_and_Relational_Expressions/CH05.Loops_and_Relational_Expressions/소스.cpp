6a5d8915d28d2a11d9c2cf4e6715c45a858204dd
Parkseojin2001 <seojin10912@naver.com>
2022-05-06 17:37:05 +0900

    Upload forstr2,cpp

--- CH05.Loops_and_Relational_Expressions/CH05.Loops_and_Relational_Expressions/소스.cpp
+++ CH05.Loops_and_Relational_Expressions/CH05.Loops_and_Relational_Expressions/소스.cpp
@@ -1,19 +1,19 @@
 #pragma warning(disable:4996)
-//block.cpp
+//forstr2.cpp
 #include <iostream>
+#include<string>
 int main() {
 	using namespace std;
-	cout << "Sum and average 5 values.\n";
-	cout << "Enter 5 values.\n";
-	double number;
-	double sum = 0.0;
-	for (int i = 1; i <= 5; i++) {
-		cout << "value " << i << ": ";
-		cin >> number;
-		sum += number;
+	cout << "Enter a word: ";
+	string word;
+	cin >> word;
+	char temp;
+	int i, j;
+	for (j = 0, i = word.size() - 1; j < i; --i, ++j) {
+		temp = word[i];
+		word[i] = word[j];
+		word[j] = temp;
 	}
-	cout << "All 5 values have been entered.\n";
-	cout << "The sum of the 5 entered values is " << sum << endl;
-	cout << "The average of the 5 entered values is " << sum / 5 << endl;
+	cout << word << "\nEnd.\n";
 	return 0;
 }
