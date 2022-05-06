7f07729e005a04ff42c912399bd6f1b82960e3d4
Parkseojin2001 <seojin10912@naver.com>
2022-05-06 16:22:11 +0900

    Upload forloop.cpp

---
+++ CH05.Loops_and_Relational_Expressions/CH05.Loops_and_Relational_Expressions/소스.cpp
@@ -0,0 +1,12 @@
+#pragma warning(disable:4996)
+//forloop.cpp
+#include <iostream>
+int main() {
+	using namespace std;
+	int i;
+	for (i = 0; i < 5; i++) {
+		cout << "Practice for_loop\n";
+	}
+	cout << "Finish for_loop\n";
+	return 0;
+}
