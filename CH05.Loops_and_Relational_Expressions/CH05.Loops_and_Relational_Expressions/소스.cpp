09c40264319fba7a454a6b2a7278089e26c41c4c
Parkseojin2001 <seojin10912@naver.com>
2022-05-06 16:43:51 +0900

    Upload express.cpp

--- CH05.Loops_and_Relational_Expressions/CH05.Loops_and_Relational_Expressions/소스.cpp
+++ CH05.Loops_and_Relational_Expressions/CH05.Loops_and_Relational_Expressions/소스.cpp
@@ -1,15 +1,20 @@
 #pragma warning(disable:4996)
-//num_test.cpp
+//express.cpp
 #include <iostream>
 int main() {
 	using namespace std;
-	cout << "Enter the count start value: ";
-	int limit;
-	cin >> limit;
-	int i;
-	for (i = limit; i; i--) {
-		cout << "i = " << i << endl;
-	}
-	cout << "i = " << i << ", so loop ends.\n";
+	int x;
+	cout << "The value of the substitution expression x = 100 is ";
+	cout << (x = 100) << endl;
+	cout << "x value = " << x << endl;
+	cout << "Relational expression x < 3 integer value :";
+	cout << (x < 3) << endl;
+	cout << "Relational expression x > 3 integer value :";
+	cout << (x > 3) << endl;
+	cout.setf(ios_base::boolalpha);
+	cout << "Relational expression x < bool value of 3 ";
+	cout << (x < 3) << endl;
+	cout << "Relational expression x > bool value of 3 ";
+	cout << (x > 3) << endl;
 	return 0;
 }
