cb56afd55c42b83cfd9d1b16856f512068624789
Parkseojin2001 <seojin10912@naver.com>
2022-05-06 16:27:27 +0900

    Upload num_test.cpp

--- CH05.Loops_and_Relational_Expressions/CH05.Loops_and_Relational_Expressions/소스.cpp
+++ CH05.Loops_and_Relational_Expressions/CH05.Loops_and_Relational_Expressions/소스.cpp
@@ -1,12 +1,15 @@
 #pragma warning(disable:4996)
-//forloop.cpp
+//num_test.cpp
 #include <iostream>
 int main() {
 	using namespace std;
+	cout << "Enter the count start value: ";
+	int limit;
+	cin >> limit;
 	int i;
-	for (i = 0; i < 5; i++) {
-		cout << "Practice for_loop\n";
+	for (i = limit; i; i--) {
+		cout << "i = " << i << endl;
 	}
-	cout << "Finish for_loop\n";
+	cout << "i = " << i << ", so loop ends.\n";
 	return 0;
 }
