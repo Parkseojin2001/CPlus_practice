2bcf0867c8f7383d0d9a080f8893ef21d0cc5ecd
Parkseojin2001 <seojin10912@naver.com>
2022-05-06 18:56:56 +0900

    Upload waiting.cpp

--- CH05.Loops_and_Relational_Expressions/CH05.Loops_and_Relational_Expressions/소스.cpp
+++ CH05.Loops_and_Relational_Expressions/CH05.Loops_and_Relational_Expressions/소스.cpp
@@ -1,18 +1,16 @@
 #pragma warning(disable:4996)
-//while.cpp
+//waiting.cpp
 #include <iostream>
-const int ArSize = 20;
+#include <ctime>
 int main() {
 	using namespace std;
-	char name[ArSize];
-	cout << "Enter your name: ";
-	cin >> name;
-	cout << "Your name, one character per line\n";
-	cout << "Shown with ASCII code like this.\n";
-	int i = 0;
-	while (name[i] != '\0') {
-		cout << name[i] << ": " << int(name[i]) << endl;
-		i++;
-	}
+	cout << "Enter the delay time in seconds: ";
+	float secs;
+	cin >> secs;
+	clock_t delay = secs * CLOCKS_PER_SEC;
+	cout << "Start counting.\a\n";
+	clock_t start = clock();
+	while (clock() - start < delay);
+	cout << "End\a\n";
 	return 0;
 }
