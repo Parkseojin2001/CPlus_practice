efe902848696fc521a3dc90c886ea2c9d80291ec
Parkseojin2001 <seojin10912@naver.com>
2022-05-06 17:31:13 +0900

    Upload block.cpp

--- CH05.Loops_and_Relational_Expressions/CH05.Loops_and_Relational_Expressions/소스.cpp
+++ CH05.Loops_and_Relational_Expressions/CH05.Loops_and_Relational_Expressions/소스.cpp
@@ -1,13 +1,19 @@
 #pragma warning(disable:4996)
-//plus_one.cpp
+//block.cpp
 #include <iostream>
 int main() {
-	using std::cout;
-	using std::endl;
-	int a = 20;
-	int b = 20;
-	cout << "a    = " << a << ":     b = " << b << endl;
-	cout << "a++  = " << a++ << ": ++b = " << ++b << endl;
-	cout << "a    = " << a << ":    b = " << b << endl;
+	using namespace std;
+	cout << "Sum and average 5 values.\n";
+	cout << "Enter 5 values.\n";
+	double number;
+	double sum = 0.0;
+	for (int i = 1; i <= 5; i++) {
+		cout << "value " << i << ": ";
+		cin >> number;
+		sum += number;
+	}
+	cout << "All 5 values have been entered.\n";
+	cout << "The sum of the 5 entered values is " << sum << endl;
+	cout << "The average of the 5 entered values is " << sum / 5 << endl;
 	return 0;
 }
