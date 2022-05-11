3a446136d4d1bbc3664fe9ecffdb35e1a015de18
Parkseojin2001 <seojin10912@naver.com>
2022-05-11 20:19:48 +0900

    Upload Quiz7 answer

--- CH05.Loops_and_Relational_Expressions/CH05.Loops_and_relation_Quiz/CH05.Loops_and_relation_Quiz/소스.cpp
+++ CH05.Loops_and_Relational_Expressions/CH05.Loops_and_relation_Quiz/CH05.Loops_and_relation_Quiz/소스.cpp
@@ -1,33 +1,28 @@
 #pragma warning(disable:4996)
 #include <iostream>
+#include <string>
+using namespace std;
+struct car {
+	string CompanyName;
+	int year;
+};
 int main() {
-	using namespace std;
-	string month[12] = {
-		"January",
-		"February",
-		"March",
-		"April",
-		"May",
-		"June",
-		"July",
-		"August",
-		"September",
-		"October",
-		"November",
-		"December"
-	};
-	int sell[3][12], sum[3] = { 0 }, total = 0;
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 12; j++) {
-			cout << month[j] << ": ";
-			cin >> sell[i][j];
-			sum[i] += sell[i][j];
-		}
-		total += sum[i];
+	int num;
+	cout << "How many cars would you like to list? ";
+	cin >> num;
+	cin.get();
+	car *list = new car[num];
+	for (int i = 0; i < num; i++) {
+		cout << "Car #" << i + 1 << ":\n";
+		cout << "Car company: ";
+		cin >> list[i].CompanyName;
+		cout << "Year of manufacture: ";
+		cin >> list[i].year;
+		cin.get();
 	}
-	for (int i = 0; i < 3; i++) {
-		cout <<i+1 << "year sales: " << sum[i] << endl;
+	cout << "Here is a list of cars you currently own.\n";
+	for (int i = 0; i < num; i++) {
+		cout << list[i].year << " " << list[i].CompanyName << endl;
 	}
-	cout << "Total sales: " << total;
 	return 0;
 }
