77b5b965bd1978a6aac39bac948348b0b2d89dcf
Parkseojin2001 <seojin10912@naver.com>
2022-05-11 19:34:08 +0900

    Upload Quiz5 answer
    Using an array of string class objects

--- CH05.Loops_and_Relational_Expressions/CH05.Loops_and_relation_Quiz/CH05.Loops_and_relation_Quiz/소스.cpp
+++ CH05.Loops_and_Relational_Expressions/CH05.Loops_and_relation_Quiz/CH05.Loops_and_relation_Quiz/소스.cpp
@@ -1,16 +1,29 @@
 #pragma warning(disable:4996)
 #include <iostream>
+#include <string>
 int main() {
 	using namespace std;
-	double A = 10000, B = 10000;
-	int year=0;
-	do {
-		A += 10000 * 0.1;
-		B += B * 0.05;
-		year++;
-	} while (A > B);
-	cout << "A's investment value: " << A << endl;
-	cout << "B's investment value: " << B << endl;
-	cout << "Year: " << year << endl;
+	string month[12] = {
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December"
+	};
+	int sell[12],sum=0;
+	for (int i = 0; i < 12; i++) {
+		cout << month[i]<<": ";
+		cin >> sell[i];
+		sum += sell[i];
+	}
+	
+	cout << "Total annual sales: " << sum << endl;
 	return 0;
 }
