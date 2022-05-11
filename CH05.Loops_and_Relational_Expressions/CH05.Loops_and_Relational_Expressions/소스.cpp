bd43a2bdb9586fd81b25c52704bb404db95b2f75
Parkseojin2001 <seojin10912@naver.com>
2022-05-11 18:19:41 +0900

    Upload nested.cpp

--- CH05.Loops_and_Relational_Expressions/CH05.Loops_and_Relational_Expressions/소스.cpp
+++ CH05.Loops_and_Relational_Expressions/CH05.Loops_and_Relational_Expressions/소스.cpp
@@ -1,14 +1,30 @@
 #pragma warning(disable:4996)
-//textin4.cpp
+//nested.cpp
 #include <iostream>
+const int Cities = 5;
+const int Years = 4;
 int main() {
 	using namespace std;
-	int ch;
-	int count = 0;
-	while ((ch = cin.get()) != EOF) {
-		cout.put(char(ch));
-		++count;
+	const char* cities[Cities] = {
+		"Seoul",
+		"Jeju",
+		"Busan",
+		"Gangneung",
+		"Daegu"
+	};
+	int maxtemps[Years][Cities] = {
+		{35,32,33,36,35},
+		{33,32,34,35,31},
+		{33,34,32,35,34},
+		{36,35,34,37,35}
+	};
+	cout << "2009~2012 highest temperature of the year\n\n";
+	for (int city = 0; city < Cities; ++city) {
+		cout << cities[city] << ":   \t";
+		for (int year = 0; year < Years; ++year) {
+			cout << maxtemps[year][city] << "\t";
+		}
+		cout << endl;
 	}
-	cout << count << " read the text.\n";
 	return 0;
 }
