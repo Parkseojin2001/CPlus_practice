1b0bef1b90427a56f8c8daff40ccbf7d1df79abb
Parkseojin2001 <seojin10912@naver.com>
2022-05-11 17:16:59 +0900

    Upload textin3.cpp

--- CH05.Loops_and_Relational_Expressions/CH05.Loops_and_Relational_Expressions/소스.cpp
+++ CH05.Loops_and_Relational_Expressions/CH05.Loops_and_Relational_Expressions/소스.cpp
@@ -1,17 +1,16 @@
 #pragma warning(disable:4996)
-//textin2.cpp
+//textin3.cpp
 #include <iostream>
 int main() {
 	using namespace std;
 	char ch;
 	int count = 0;
-	cout << "Enter text; Enter # to end:\n";
 	cin.get(ch);
-	while (ch != '#') {
+	while (cin.fail() == false) {
 		cout << ch;
 		++count;
 		cin.get(ch);
 	}
-	cout << endl << count << " read the text.\n";
+	cout << count << " read the text.\n";
 	return 0;
 }
