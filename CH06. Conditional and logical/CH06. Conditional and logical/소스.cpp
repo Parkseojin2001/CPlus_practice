df82d8fd0261ea863850de33c2fcc47688c5d544
Parkseojin2001 <seojin10912@naver.com>
2022-05-13 19:37:32 +0900

    Upload condit.cpp

--- CH06. Conditional and logical/CH06. Conditional and logical/소스.cpp
+++ CH06. Conditional and logical/CH06. Conditional and logical/소스.cpp
@@ -1,35 +1,13 @@
 #pragma warning(disable:4996)
 #include<iostream>
-#include<cctype>
-//cctypes.cpp
+//condit.cpp
 int main() {
 	using namespace std;
-	cout << "Enter text to be analyzed. "
-		"End of the input with @\n";
-	char ch;
-	int whitespace = 0;
-	int digits = 0;
-	int chars = 0;
-	int punct = 0;
-	int others = 0;
-	cin.get(ch);
-	while (ch != '@') {
-		if (isalpha(ch))
-			chars++;
-		else if (isspace(ch))
-			whitespace++;
-		else if (isdigit(ch))
-			digits++;
-		else if (ispunct(ch))
-			punct++;
-		else
-			others++;
-		cin.get(ch);
-	}
-	cout << "Char " << chars << ", "
-		<< "Whitespace " << whitespace << ", "
-		<< "Digit " << digits << ", "
-		<< "Punct " << punct << ", "
-		<< "Others " << others << endl;
+	int a, b;
+	cout << "Enter two intgers: ";
+	cin >> a >> b;
+	cout << "The greater of thw two is ";
+	int c = a > b ? a : b;
+	cout << c << endl;
 	return 0;
 }
