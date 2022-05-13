f6cbefbc053c3bc1a4f8e67d24ac674d4e447a04
Parkseojin2001 <seojin10912@naver.com>
2022-05-13 18:57:11 +0900

    Upload more_and.cpp

--- CH06. Conditional and logical/CH06. Conditional and logical/소스.cpp
+++ CH06. Conditional and logical/CH06. Conditional and logical/소스.cpp
@@ -1,37 +1,26 @@
 #pragma warning(disable:4996)
 #include<iostream>
-//and.cpp
-const int ArSize = 6;
+//more_and.cpp
+const char* qualify[4] = {
+	"10000m Run",
+	"Sand Wrestling",
+	"Beach Wolley",
+	"Boomerang Throw"
+};
 int main() {
 	using namespace std;
-	float naaq[ArSize];
-	cout << "Enter your colleagues'New Age Aareness Quotients.\n"
-		<< ArSize << "If you entered all data or a negative number\n"
-		<< "escape while loop.\n";
-	int i = 0;
-	float temp;
-	cout << "First value: ";
-	cin >> temp;
-	while (i < ArSize && temp >= 0) {
-		naaq[i] = temp;
-		++i;
-		if (i < ArSize) {
-			cout << "Next value: ";
-			cin >> temp;
-		}
-	}
-	if (i == 0)
-		cout << "There is no data entered, so the program is terminated\n";
-	else {
-		cout << "Enter your NAAQ: ";
-		float you;
-		cin >> you;
-		int count = 0;
-		for (int j = 0; j < i; j++)
-			if (naaq[j] > you)
-				++count;
-		cout << "Among colleagues " << count;
-		cout << " people is better than your NAAQ.\n";
-	}
+	int age;
+	cout << "Enter your age: ";
+	cin >> age;
+	int index;
+	if (age > 17 && age < 35)
+		index = 0;
+	else if (age >= 35 && age < 50)
+		index = 1;
+	else if (age >= 50 && age < 65)
+		index = 2;
+	else
+		index = 3;
+	cout << "You can participate in " << qualify[index] << ".\n";
 	return 0;
 }
