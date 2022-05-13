13bba8930941b4a9fc2180ba7be9b5c04ef3878d
Parkseojin2001 <seojin10912@naver.com>
2022-05-13 19:55:29 +0900

    Upload switch.cpp

--- CH06. Conditional and logical/CH06. Conditional and logical/소스.cpp
+++ CH06. Conditional and logical/CH06. Conditional and logical/소스.cpp
@@ -1,13 +1,43 @@
 #pragma warning(disable:4996)
 #include<iostream>
-//condit.cpp
+//switch.cpp
+using namespace std;
+void showmenu();
+void report();
+void comfort();
 int main() {
-	using namespace std;
-	int a, b;
-	cout << "Enter two intgers: ";
-	cin >> a >> b;
-	cout << "The greater of thw two is ";
-	int c = a > b ? a : b;
-	cout << c << endl;
+	showmenu();
+	int choice;
+	cin >> choice;
+	while (choice != 5) {
+		switch (choice) {
+		case 1:cout << "\a\n";
+			break;
+		case 2:report();
+			break;
+		case 3:cout << "The boss was at the company today.\n";
+			break;
+		case 4:comfort();
+			break;
+		default:cout << "It cannot be selected.\n";
+		}
+		showmenu();
+		cin >> choice;
+	}
+	cout << "Quit the program!\n";
 	return 0;
+}
+void showmenu() {
+	cout << "Choose one from 1, 2, 3, 4, 5:\n"
+		"1) Alarm                 2) Report\n"
+		"3) Alibi                 4) Encouragement\n"
+		"5) Quit\n";
+}
+void report() {
+	cout << "This quarter's business performance is very good\n"
+		"Sales increased by 120% and costs decreased by 35%.\n";
+}
+void comfort() {
+	cout << "The employees think you are the best CEO in the industry\n"
+		"The board thinks you are the best CEO in the industry\n";
 }
