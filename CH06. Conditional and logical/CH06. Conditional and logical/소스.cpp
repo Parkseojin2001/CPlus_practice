0e838e9a5b9f80c46ba2f550f5e8b2fc38084dc7
Parkseojin2001 <seojin10912@naver.com>
2022-05-13 18:37:26 +0900

    Upload and.cpp

--- CH06. Conditional and logical/CH06. Conditional and logical/소스.cpp
+++ CH06. Conditional and logical/CH06. Conditional and logical/소스.cpp
@@ -1,20 +1,37 @@
 #pragma warning(disable:4996)
 #include<iostream>
-//or.cpp
+//and.cpp
+const int ArSize = 6;
 int main() {
 	using namespace std;
-	cout << "This program will reformat the hard disk\n"
-		"All data on the hard disk will be erased.\n"
-		"Do you want to continue? <y/n> ";
-	char ch;
-	cin >> ch;
-	if (ch == 'y' || ch == 'Y')
-		cout << "I have clearly warned you.\a\a\n";
-	else if (ch == 'n' || ch == 'N')
-		cout << "This is a wise choice. Quit the program.\n";
-	else
-		cout << "You did not enter y or n.\n"
-		"Could not follow instruction. "
-		"I'll empty the Recycle Kin and clean the disk.\n";
+	float naaq[ArSize];
+	cout << "Enter your colleagues'New Age Aareness Quotients.\n"
+		<< ArSize << "If you entered all data or a negative number\n"
+		<< "escape while loop.\n";
+	int i = 0;
+	float temp;
+	cout << "First value: ";
+	cin >> temp;
+	while (i < ArSize && temp >= 0) {
+		naaq[i] = temp;
+		++i;
+		if (i < ArSize) {
+			cout << "Next value: ";
+			cin >> temp;
+		}
+	}
+	if (i == 0)
+		cout << "There is no data entered, so the program is terminated\n";
+	else {
+		cout << "Enter your NAAQ: ";
+		float you;
+		cin >> you;
+		int count = 0;
+		for (int j = 0; j < i; j++)
+			if (naaq[j] > you)
+				++count;
+		cout << "Among colleagues " << count;
+		cout << " people is better than your NAAQ.\n";
+	}
 	return 0;
 }
