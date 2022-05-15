6fe08ba73848a63b79861c4f5dd0670ccfdda13b
Parkseojin2001 <seojin10912@naver.com>
2022-05-15 17:02:24 +0900

    Upload jump.cpp

--- CH06. Conditional and logical/CH06. Conditional and logical/소스.cpp
+++ CH06. Conditional and logical/CH06. Conditional and logical/소스.cpp
@@ -1,25 +1,24 @@
 #pragma warning(disable:4996)
 #include<iostream>
-//enum.cpp
-enum{red, orange,yellow,green,blue,violet,indigo};
+//jump.cpp
+const int ArSize = 80;
 int main() {
 	using namespace std;
-	cout << "Enter color code(0,1,2,3,4,5,6): ";
-	int code;
-	cin >> code;
-	while (code >= red && code <= indigo) {
-		switch (code) {
-		case red: cout << "Lips were red.\n"; break;
-		case orange:cout << "Hair was orange.\n"; break;
-		case yellow:cout << "Shoes were yellow.\n"; break;
-		case green:cout << "Fingernail was green.\n"; break;
-		case blue:cout << "Sweater was blue.\n"; break;
-		case violet:cout << "Snow was vioet.\n"; break;
-		case indigo: cout << "Atmosphere was indigo.\n"; break;
-		}
-		cout << "Enter color code(0,1,2,3,4,5,6): ";
-		cin >> code;
+	char line[ArSize];
+	int spaces = 0;
+	cout << "Enter text:\n";
+	cin.get(line, ArSize);
+	cout << "All text:\n" << line << endl;
+	cout << "Text to first period:\n";
+	for (int i = 0; line[i] != '\0'; i++) {
+		cout << line[i];
+		if (line[i] == '.')
+			break;
+		if (line[i] != ' ')
+			continue;
+		spaces++;
 	}
-	cout << "Quit the program.\n";
+	cout << "\nBlank is " << spaces << endl;
+	cout << "Exit\n";
 	return 0;
 }
