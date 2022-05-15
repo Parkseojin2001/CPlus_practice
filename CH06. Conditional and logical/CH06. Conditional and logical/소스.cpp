84b4c3ee4ba577419a25363dedf373f715dbdee1
Parkseojin2001 <seojin10912@naver.com>
2022-05-15 18:05:39 +0900

    Upload cingolf.cpp

--- CH06. Conditional and logical/CH06. Conditional and logical/소스.cpp
+++ CH06. Conditional and logical/CH06. Conditional and logical/소스.cpp
@@ -1,26 +1,25 @@
 #pragma warning(disable:4996)
 #include<iostream>
-//cinfish.cpp
+//cingolf.cpp
 const int Max = 5;
 int main() {
 	using namespace std;
-	double fish[Max];
-	cout << "Enter the weight of the fish you caught today.\n";
-	cout << "You can catch up to " << Max << "fish.\n"
-		<< "<If you end the input, press P.>\n";
-	cout << "fish #1: ";
-	int i = 0;
-	while (i < Max && cin >> fish[i]) {
-		if (++i < Max)
-			cout << "fish #" << i + 1 << ": ";
+	int golf[Max];
+	cout << "Enter golf score.\n";
+	cout << "A total of " << Max << " rounds must be entered.\n";
+	int i;
+	for (i = 0; i < Max; i++) {
+		cout << "round #" << i + 1 << ": ";
+		while (!(cin >> golf[i])) {
+			cin.clear();
+			while (cin.get() != '\n')
+				continue;
+			cout << "Enter golf score: ";
+		}
 	}
 	double total = 0.0;
-	for (int j = 0; j < i; j++)
-		total += fish[j];
-	if (i == 0)
-		cout << "You haven't caught a single fish.\n";
-	else
-		cout << "Fish " << i << "average weight of the mare is " << total / i << "gram.\n";
-	cout << "Exit program.\n";
+	for (i = 0; i < Max; i++)
+		total += golf[i];
+	cout << "Average score of total of " << Max << " rounds = " << total / Max << endl;
 	return 0;
 }
