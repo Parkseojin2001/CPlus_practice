b392d8c877f6181474cb565b44d1a0778ad9c341
Parkseojin2001 <seojin10912@naver.com>
2022-05-17 11:33:21 +0900

    Upload assignment3-3

--- Assignment3/Assignment3/소스.cpp
+++ Assignment3/Assignment3/소스.cpp
@@ -1,35 +1,32 @@
 #pragma warning(disable:4996)
 #include <iostream>
-//Quiz2
+//Quiz3
 using namespace std;
-class Area {    //Class to find the area of a rectangle
+class shape { //create class for width and height
+protected:
+	int width = 10;
+	int height = 5;
+};
+class rectangle :public shape {  //create class that prints the area of a rectangle
 public:
-	double area(double a,double b) {  //define a function to calculate the area
-		return a * b;
+	void cal_r() { //Define function to print the area of a rectangle
+		int area_r;  //Declaration variable to store the area
+		area_r = width * height;   //calculate the area of a rectangle
+		cout << "Area of area_r: " << area_r << endl;   //print the area of a rectangle
 	}
 };
-class Perimeter { //Class to find the perimeter of a rectangle
+class triangle :public shape {  //create class that prints the area of a triangle
 public:
-	double perimeter(double a,double b) {  //define a function to calculate the perimeter
-		return 2 * (a + b);
-	}
-};
-class rectangle :public Area, public Perimeter { //Class to print area and perimeter
-	double a, p,n,m;   //variable declaration 
-	Area A;   // declaration object
-	Perimeter P;
-public:
-	void show() {   //define a function to show area and perimeter
-		cin >> n;  //Enter horizontal
-		cin >> m;  //Enter length
-		a = A.area(n,m);
-		p = P.perimeter(n,m);
-		cout << "Area: " << a << endl;   //Print rectangle area
-		cout << "Perimeter " << p << endl;   //Print rectangle area
+	void cal_t() {   //Define function to print the area of a rectangle
+		int area_t;  //Declaration valriable to store the area
+		area_t = width * height / 2;  //calculate the area of a triangle
+		cout << "Area of triangle: " << area_t << endl; //print the area of a rectangle
 	}
 };
 int main() {
-	rectangle r;   //declaration object
-	r.show();   //Call a function to show rectangle's area and perimeter
+	rectangle A;  //declaration object
+	triangle B;
+	A.cal_r();  //call function to print the area of a rectangle
+	B.cal_t(); //call function to print the area of a triangle
 	return 0;
 }
