29717f035d5e00d743482fc89031c122c25b0c56
Parkseojin2001 <seojin10912@naver.com>
2022-05-19 19:23:34 +0900

    Upload Quiz2 answer

--- CH06. Conditional and logical/CH06.Conditional and logical_Quiz/CH06.Conditional and logical_Quiz/소스.cpp
+++ CH06. Conditional and logical/CH06.Conditional and logical_Quiz/CH06.Conditional and logical_Quiz/소스.cpp
@@ -1,18 +1,23 @@
 #pragma warning(disable:4996)
 #include<iostream>
-#include<cctype>
+#include<array>
 int main() {
 	using namespace std;
-	char ch;
-	do {
-		cout << "Enter character: ";
-		cin >> ch;
-		if (islower(ch)) {
-			ch = toupper(ch);
-		}
-		else if (isupper(ch)) {
-			ch = tolower(ch);
-		}
-		cout << ch << endl;
-	} while (ch != '@');
+	double sum = 0;
+	array<double, 10>donation;
+	for (int i = 0; i < 10; i++) {
+		cout << "Enter donation #" << i+1 << endl;
+		if (!(cin >> donation[i]))
+			exit(EXIT_FAILURE);
+		sum += donation[i];
+	}
+	double avg = sum / 10;
+	int count = 0;
+	for (int i = 0; i < 10; i++) {
+		if (avg < donation[i])
+			count++;
+	}
+	cout << "Average donation: " << avg << endl;
+	cout << "Number of donation greater than average: " << count << endl;
+	return 0;
 }
