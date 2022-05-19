ec7e736cf54adc4c64cd3ad42ead3e503d2f2a65
Parkseojin2001 <seojin10912@naver.com>
2022-05-19 20:36:21 +0900

    Upload Quiz5 answer

--- CH06. Conditional and logical/CH06.Conditional and logical_Quiz/CH06.Conditional and logical_Quiz/소스.cpp
+++ CH06. Conditional and logical/CH06.Conditional and logical_Quiz/CH06.Conditional and logical_Quiz/소스.cpp
@@ -1,56 +1,22 @@
 #pragma warning(disable:4996)
 #include<iostream>
-#include<string>
-using namespace std;
-struct bop {
-	string fullname;
-	string title;
-	string bopname;
-	int prefernece;
-};
 int main() {
-	bop list[5] = {
-		{"Wimp Macho","CEO","AAA",0},
-		{"Raki Rhodes","Junior Programmer","BBB",1},
-		{"Celia Laiter","doctor","MIPS",2},
-		{"Hoppy Hipman","Analyst Trainee","DDD",1},
-		{"Pat Hand","Engineer","LOPPY",2}
-	};
-	char c;
-	cout << "Benevolent Order of Programmers\n"
-		<< "a. Reading by full name             b. Reading by title\n"
-		<< "c. Reading by BOP_ID                d. Reading by preference\n"
-		<< "q. Exit\n";
-	do {
-		cout << "Choose what you want: ";
-		cin >> c;
-		switch (c) {
-		case 'a':
-			for (int i = 0; i < 5; i++)
-				cout << list[i].fullname << endl;
-			break;
-		case 'b':
-			for (int i = 0; i < 5; i++)
-				cout << list[i].title << endl;
-			break;
-		case'c':
-			for (int i = 0; i < 5; i++)
-				cout << list[i].bopname << endl;
-			break;
-		case'd':
-			for (int i = 0; i < 5; i++) {
-				if (list[i].prefernece == 0)
-					cout << list[i].fullname << endl;
-				else if (list[i].prefernece == 1)
-					cout << list[i].title << endl;
-				else
-					cout << list[i].bopname << endl;
-			}
-			break;
-		case 'q':
-			cout << "End the program.\n";
-			break;
+	using namespace std;
+	double income, tax = 0;
+	cout << "Enter your income: ";
+	while (cin >> income && income > 0) {
+		if (income <= 5000)
+			tax = 0;
+		else if (income > 5000 && income <= 10000)
+			tax += income * 0.1;
+		else if (income>10000 && income <= 20000)
+			tax += income * 0.15;
+		else if(income >= 35000) {
+			income -= 35000;
+			tax += income * 0.2+4000;
 		}
-	} while (c != 'q');
+		cout << "Tax: " << tax << endl;
+		cout << "Enter your income: ";
+	}
 	return 0;
 }
