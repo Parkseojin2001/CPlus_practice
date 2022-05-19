bad75cd6e76f45afdfe4febebdd43de2001e001c
Parkseojin2001 <seojin10912@naver.com>
2022-05-19 19:34:52 +0900

    Upload Quiz3 answer

--- CH06. Conditional and logical/CH06.Conditional and logical_Quiz/CH06.Conditional and logical_Quiz/소스.cpp
+++ CH06. Conditional and logical/CH06.Conditional and logical_Quiz/CH06.Conditional and logical_Quiz/소스.cpp
@@ -3,21 +3,26 @@
 #include<array>
 int main() {
 	using namespace std;
-	double sum = 0;
-	array<double, 10>donation;
-	for (int i = 0; i < 10; i++) {
-		cout << "Enter donation #" << i+1 << endl;
-		if (!(cin >> donation[i]))
-			exit(EXIT_FAILURE);
-		sum += donation[i];
+	cout << "Select one of the following options. (If you want to exit, enter q)\n"
+		<< "c) camera        p) pianist\n"
+		<< "t) tree          g) game\n";
+	char ch;
+	cin >> ch;
+	while (ch != 'q') {
+		switch(ch){
+		case 'c': cout << "Select camera\n";
+			break;
+		case 'p': cout << "Select pianist\n";
+			break;
+		case 't': cout << "Select tree\n";
+			break;
+		case 'g': cout << "Select game\n";
+			break;
+		default: cout << "Select one from c, p, t, and g. (If you want to exit, enter q) : ";
+			cin >> ch;
+			continue;
+		}
+		break;
 	}
-	double avg = sum / 10;
-	int count = 0;
-	for (int i = 0; i < 10; i++) {
-		if (avg < donation[i])
-			count++;
-	}
-	cout << "Average donation: " << avg << endl;
-	cout << "Number of donation greater than average: " << count << endl;
 	return 0;
 }
