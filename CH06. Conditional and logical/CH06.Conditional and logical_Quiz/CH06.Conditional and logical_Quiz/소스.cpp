fa112cd93c518be7c31a6540bd78cc6cdb664a10
Parkseojin2001 <seojin10912@naver.com>
2022-05-19 21:00:52 +0900

    Upload Quiz6 answer

--- CH06. Conditional and logical/CH06.Conditional and logical_Quiz/CH06.Conditional and logical_Quiz/소스.cpp
+++ CH06. Conditional and logical/CH06.Conditional and logical_Quiz/CH06.Conditional and logical_Quiz/소스.cpp
@@ -1,22 +1,43 @@
 #pragma warning(disable:4996)
 #include<iostream>
+#include<string>
+using namespace std;
+struct info{
+	string name;
+	double donation;
+};
 int main() {
-	using namespace std;
-	double income, tax = 0;
-	cout << "Enter your income: ";
-	while (cin >> income && income > 0) {
-		if (income <= 5000)
-			tax = 0;
-		else if (income > 5000 && income <= 10000)
-			tax += income * 0.1;
-		else if (income>10000 && income <= 20000)
-			tax += income * 0.15;
-		else if(income >= 35000) {
-			income -= 35000;
-			tax += income * 0.2+4000;
-		}
-		cout << "Tax: " << tax << endl;
-		cout << "Enter your income: ";
+	int n;
+	cout << "Society for the Preservation of Rightful Influence\n";
+	cout << "Number of people to donate: ";
+	cin >> n;
+	info *list = new info[n];
+	for (int i = 0; i < n; i++) {
+		cout << i+1 << " Donor Name: ";
+		cin >> list[i].name;
+		cout << "Donation amount: ";
+		cin >> list[i].donation;
 	}
+	int cnt = 0;
+	cout << "\nA list of big donors\n\n";
+	for (int i = 0; i < n; i++) {
+		if (list[i].donation >= 10000) {
+			cout << cnt + 1 << ". " << list[i].name << "      " << list[i].donation << endl;
+			cnt++;
+		}
+	}
+	if (cnt == 0)
+		cout << "\nWe don't have a donor\n";
+	cnt = 0;
+	cout << "\nA list small donors\n\n";
+	for (int i = 0; i < n; i++) {
+		if (list[i].donation < 10000) {
+			cout << cnt + 1 << ". " << list[i].name << "      " << list[i].donation << endl;
+			cnt++;
+		}
+	}
+	if(cnt==0)
+		cout << "\nWe don't have a donor\n";
 	return 0;
+
 }
