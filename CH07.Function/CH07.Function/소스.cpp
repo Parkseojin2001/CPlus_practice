7a0dd14d52fb17417c0508bfb72438ca0fbbf89b
Parkseojin2001 <seojin10912@naver.com>
2022-05-27 21:19:06 +0900

    Upload arrfun2.cpp

--- CH07.Function/CH07.Function/소스.cpp
+++ CH07.Function/CH07.Function/소스.cpp
@@ -1,17 +1,24 @@
 #pragma warning(disable:4996)
 #include <iostream>
-//arrfun1.cpp
+//arrfun2.cpp
 const int ArSize = 8;
 int sum_arr(int arr[], int n);
 int main() {
-	using namespace std;
-	int cookies[ArSize] = { 1,2,4,8,16,32,64,128 };
+	int cookies[ArSize] = {1,2,4,8,16,32,64,128};
+	std::cout << cookies << " = array address, ";
+	std::cout << "sizeof cookies = " << sizeof cookies << std::endl;
 	int sum = sum_arr(cookies, ArSize);
-	cout << "Number of cookies eaten: " << sum << endl;
+	std::cout << "Total of cookies eaten: " << sum << std::endl;
+	sum = sum_arr(cookies, 3);
+	std::cout << "Three people ate " << sum << "cookies.\n";
+	sum = sum_arr(cookies + 4, 4);
+	std::cout << "Four people ate " << sum << "cookies.\n";
 	return 0;
 }
 int sum_arr(int arr[], int n) {
 	int total = 0;
+	std::cout << arr << " = arr,";
+	std::cout << "sizeof arr = " << sizeof arr << std::endl;
 	for (int i = 0; i < n; i++) {
 		total = total + arr[i];
 	}
