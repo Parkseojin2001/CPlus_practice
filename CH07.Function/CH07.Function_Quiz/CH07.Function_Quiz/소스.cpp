2cf449f68d439f067360a84bab78a2b3d7d489f7
Parkseojin2001 <seojin10912@naver.com>
2022-06-02 20:13:20 +0900

    Upload Quiz2 answer

--- CH07.Function/CH07.Function_Quiz/CH07.Function_Quiz/소스.cpp
+++ CH07.Function/CH07.Function_Quiz/CH07.Function_Quiz/소스.cpp
@@ -1,22 +1,45 @@
 #pragma warning(disable:4996)
 #include <iostream>
-//Quiz1
-double avg(double, double);
+//Quiz2
+const int cnt = 10;
+void input(double *score);
+void output(double *score);
+double avg(double *score);
 int main() {
-	using namespace std;
-	double a, b,value;
-	while (1) {
-		cout << "Enter two number: ";
-		cin >> a >> b;
-		if (a == 0 || b == 0)
-			break;
-		value = avg(a, b);
-		cout << "Answer: " << value << endl;
-	}
+	double score[cnt];
+	input(score);
+	output(score);
 	return 0;
 }
-double avg(double x, double y) {
-	double result;
-	result = 2.0 * x * y / (x + y);
-	return result;
+void input(double *score) {
+	using namespace std;
+	double a;
+	for (int i = 0; i < cnt; i++) {
+		cout << i+1 << "'s game score: ";
+		cin >> a;
+		if (a < 0)
+			break;
+		score[i] = a;
+	}
+}
+void output(double* score) {
+	using namespace std;
+	double Avg;
+	Avg = avg(score);
+	cout << "Golf game score: ";
+	for (int i = 0; i < cnt && score[i] >= 0; i++) {
+		cout << score[i] << " ";
+	}
+	cout << "\n";
+	cout << "Average of Golf score: " << Avg << endl;
+}
+double avg(double* score) {
+	double sum = 0,AVG;
+	int count = 0;
+	for (int i = 0; i < cnt && score[i] >= 0; i++) {
+		sum += score[i];
+		count++;
+	}
+	AVG = sum / count;
+	return AVG;
 }
