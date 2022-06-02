6128216ce334c63de893a62aafb22acee117b76c
Parkseojin2001 <seojin10912@naver.com>
2022-06-02 21:44:10 +0900

    Upload Quiz6 answer

--- CH07.Function/CH07.Function_Quiz/CH07.Function_Quiz/소스.cpp
+++ CH07.Function/CH07.Function_Quiz/CH07.Function_Quiz/소스.cpp
@@ -1,22 +1,38 @@
 #pragma warning(disable:4996)
 #include <iostream>
-//Quiz5
-int factorial(int);
+//Quiz6
+int Fill_array(double*, int);
+void Show_array(double*, int);
+void Reverse_array(double*, int);
 int main() {
 	using namespace std;
-	int N;;
-	while (1) {
-		cout << "Enter number: ";
-		if (!(cin >> N))
-			break;
-		cout << N << "! = " << factorial(N) << endl;
-	}
-	return 0;
+	double ar[10];
+	int n = Fill_array(ar, 10);
+	Show_array(ar, n);
+	cout << "REVERSE" << endl;
+	Reverse_array(ar, n);
+	Show_array(ar, n);
 }
-int factorial(int n) {
-	if (n > 0) {
-		return n * factorial(n - 1);
+int Fill_array(double* arr, int n) {
+	using namespace std;
+	int i;
+	for (i = 0; i < n; i++) {
+		cout << i + 1 << "'s double value: ";
+		if (!(cin >> arr[i]))
+			break;
 	}
-	else
-		return 1;
+	return i;
+}
+void Show_array(double* ar, int n) {
+	using namespace std;
+	for (int i = 0; i < n; i++)
+		cout << i + 1 << " : " << ar[i] << endl;
+}
+void Reverse_array(double* ar, int n) {
+	for (int i = 0; i < n / 2; i++) {
+		int temp;
+		temp = ar[i];
+		ar[i] = ar[n - 1 - i];
+		ar[n - 1 - i] = temp;
+	}
 }
