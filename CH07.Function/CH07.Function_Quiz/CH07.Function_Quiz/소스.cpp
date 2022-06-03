5c0fd4a3ede3201fa72fd890423c89bdd7c291bf
Parkseojin2001 <seojin10912@naver.com>
2022-06-03 20:36:01 +0900

    Upload Quiz9 answer

--- CH07.Function/CH07.Function_Quiz/CH07.Function_Quiz/소스.cpp
+++ CH07.Function/CH07.Function_Quiz/CH07.Function_Quiz/소스.cpp
@@ -2,35 +2,63 @@
 #include <iostream>
 #include <string>
 #include <array>
-//Quiz8-b
-const int Seasons = 4;
-const char* Sname[4] = { "Spring", "Summer","Fall","Winter" };
-void fill(struct a *pa);
-void show(struct a da);
-struct a {
-	double ex[Seasons];
+//Quiz9
+using namespace std;
+const int SLEN = 30;
+struct student {
+	char fullname[SLEN];
+	char hobby[SLEN];
+	int ooplevel;
 };
+int getinfo(student pa[], int n);
+void display1(student st);
+void display2(const student* ps);
+void display3(const student pa[], int n);
 int main() {
-	using namespace std;
-	struct a expenses;
-	fill(&expenses);
-	show(expenses);
+	cout << "Enter the number of students in the class: ";
+	int class_size;
+	cin >> class_size;
+	while (cin.get() != '\n')
+		continue;
+	student* ptr_stu = new student[class_size];
+	int entered = getinfo(ptr_stu, class_size);
+	for (int i = 0; i < entered; i++) {
+		display1(ptr_stu[i]);
+		display2(&ptr_stu[i]);
+	}
+	display3(ptr_stu, entered);
+	cout << "Exit program.\n";
 	return 0;
 }
-void fill(struct a *pa) {
-	using namespace std;
-	for (int i = 0; i < Seasons; i++) {
-		cout << "Cost in " << Sname[i] << endl;
-		cin >> pa->ex[i];
+int getinfo(student pa[], int n) {
+	int i;
+	for (i = 0; i < n; i++) {
+		cout << "Student Name:";
+		cin.getline(pa[i].fullname, SLEN);
+		if (pa[i].fullname[0] == '\n')
+			break;
+		cout << "Hobby: ";
+		cin.getline(pa[i].hobby, SLEN);
+		cout << "ooplevel: ";
+		cin >> pa[i].ooplevel;
+		cin.get();
 	}
+	return i;
 }
-void show(struct a da) {
-	using namespace std;
-	double total = 0.0;
-	cout << "\nSeasonal cost\n";
-	for (int i = 0; i < Seasons; i++) {
-		cout << Sname[i] << " : $" << da.ex[i] << endl;
-		total += da.ex[i];
+void display1(student st) {
+	cout << st.fullname << " "
+		<< st.hobby << " "
+		<< st.ooplevel << endl;
+}
+void display2(const student* ps) {
+	cout << ps->fullname << " "
+		<< ps->hobby << " "
+		<< ps->ooplevel << endl;
+}
+void display3(const student pa[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << pa[i].fullname << " "
+			<< pa[i].hobby << " "
+			<< pa[i].ooplevel << endl;
 	}
-	cout << "Total cost : $" << total << endl;
 }
