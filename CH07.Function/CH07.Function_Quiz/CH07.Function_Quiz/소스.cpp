2c0fb7e16d9a3cf7ee5d6da95e83524c7486b5c3
Parkseojin2001 <seojin10912@naver.com>
2022-06-03 21:02:13 +0900

    Upload Quiz10 answer

--- CH07.Function/CH07.Function_Quiz/CH07.Function_Quiz/소스.cpp
+++ CH07.Function/CH07.Function_Quiz/CH07.Function_Quiz/소스.cpp
@@ -1,64 +1,30 @@
 #pragma warning(disable:4996)
 #include <iostream>
-#include <string>
-#include <array>
-//Quiz9
-using namespace std;
-const int SLEN = 30;
-struct student {
-	char fullname[SLEN];
-	char hobby[SLEN];
-	int ooplevel;
-};
-int getinfo(student pa[], int n);
-void display1(student st);
-void display2(const student* ps);
-void display3(const student pa[], int n);
+//Quiz10
+double calculate(double x, double y, double(*cal)(double, double));
+double add(double x, double y);
+double min(double x, double y);
 int main() {
-	cout << "Enter the number of students in the class: ";
-	int class_size;
-	cin >> class_size;
-	while (cin.get() != '\n')
-		continue;
-	student* ptr_stu = new student[class_size];
-	int entered = getinfo(ptr_stu, class_size);
-	for (int i = 0; i < entered; i++) {
-		display1(ptr_stu[i]);
-		display2(&ptr_stu[i]);
-	}
-	display3(ptr_stu, entered);
-	cout << "Exit program.\n";
-	return 0;
-}
-int getinfo(student pa[], int n) {
-	int i;
-	for (i = 0; i < n; i++) {
-		cout << "Student Name:";
-		cin.getline(pa[i].fullname, SLEN);
-		if (pa[i].fullname[0] == '\n')
+	using namespace std;
+	double (*pf[2])(double, double) = { add,min };
+	while (1) {
+		double x, y;
+		int n;
+		cout << "Enter two number(If you want to exit,enter any character.): ";
+		if (!(cin >> x >> y))
 			break;
-		cout << "Hobby: ";
-		cin.getline(pa[i].hobby, SLEN);
-		cout << "ooplevel: ";
-		cin >> pa[i].ooplevel;
-		cin.get();
+		cout << "Plus: 0        MInus: 1\n";
+		cin >> n;
+		double q = calculate(x, y, *pf[n]);
+		cout << "Result: " << q << endl;
 	}
-	return i;
 }
-void display1(student st) {
-	cout << st.fullname << " "
-		<< st.hobby << " "
-		<< st.ooplevel << endl;
+double calculate(double x, double y, double(*cal)(double, double)) {
+	return (*cal)(x, y);
 }
-void display2(const student* ps) {
-	cout << ps->fullname << " "
-		<< ps->hobby << " "
-		<< ps->ooplevel << endl;
+double add(double x, double y) {
+	return x+y;
 }
-void display3(const student pa[], int n) {
-	for (int i = 0; i < n; i++) {
-		cout << pa[i].fullname << " "
-			<< pa[i].hobby << " "
-			<< pa[i].ooplevel << endl;
-	}
+double min(double x, double y) {
+	return x - y;
 }
