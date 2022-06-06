878ce5b0003ed0c45fc952258d59dbea936a4933
Parkseojin2001 <seojin10912@naver.com>
2022-06-06 21:44:44 +0900

    Create Assignment5

--- Assignment4/Assignment4/소스.cpp
+++ Assignment4/Assignment4/소스.cpp
@@ -3,13 +3,13 @@
 #include<string>
 #include<array>
 using namespace std;
-struct student {
-	string name;
-	int cplus, physics;
+struct student {  //Declaration struct for student information
+	string name;  //Student name variable
+	int cplus, physics;  //Student subject score variable
 };
 int main() {
-	array<struct student,3>info;
-	info[0].name = "Student A";
+	array<struct student,3>info;   //Use array
+	info[0].name = "Student A";   //struct array initialization
 	info[0].cplus = 20;
 	info[0].physics = 100;
 	info[1].name = "Student B";
@@ -19,26 +19,26 @@ int main() {
 	info[2].cplus = 30;
 	info[2].physics = 50;
 	int order;
-	cout << "1.Show All student information  2.Show one student information  3.Exit\n";
+	cout << "1.Show All student information  2.Show one student information  3.Exit\n"; //Print menu that can be operated
 	while (1) {
 		cout << "Enter the menu number:";
-		cin >> order;
-		if (order == 1)
+		cin >> order;   //Select menu
+		if (order == 1)   //Show all student information
 			for (int i = 0; i < 3; i++) {
-				cout << "Student Name: " <<info[i].name << endl;
-				cout << "Student C++ score: " << info[i].cplus << endl;
-				cout << "Studnet Physics score: " << info[i].physics << endl;
+				cout << "Student Name: " <<info[i].name << endl;  //Print student name
+				cout << "Student C++ score: " << info[i].cplus << endl;   //Print student c++ score
+				cout << "Studnet Physics score: " << info[i].physics << endl;  //Print student physics score
 			}
-		else if (order == 2) {
+		else if (order == 2) {   //Show one student information
 			int N;
 			cout << "Select Student number: ";
-			cin >> N;
-			cout << "Student Name: " << info[N - 1].name << endl;
-			cout << "Student C++ score: " << info[N - 1].cplus << endl;
-			cout << "Studnet Physics score: " << info[N - 1].physics << endl;
+			cin >> N;   //Enter the student number for which you want to print information
+			cout << "Student Name: " << info[N - 1].name << endl;  //Print student name
+			cout << "Student C++ score: " << info[N - 1].cplus << endl;  //Print student c++ score
+			cout << "Studnet Physics score: " << info[N - 1].physics << endl;  //Print student physics score
 		}
 		else {
-			cout << "-------- Exit--------";
+			cout << "-------- Exit--------";  // Exit program
 			break;
 		}
 	}
