84d7e01bbedfe1528bc1c07901f2b615a399e51b
Parkseojin2001 <seojin10912@naver.com>
2022-06-16 04:05:25 +0900

    Update Function Templates
    Argument of Different Types

--- Final exam practice/Final exam practice/소스.cpp
+++ Final exam practice/Final exam practice/소스.cpp
@@ -1,15 +1,16 @@
 #pragma warning(disable:4996)
 #include <iostream>
 using namespace std;
-template<class t>
-t big(t a, t b) {
+template<class t1,class t2>  //The data types of the two classes are different
+float big(t1 a, t2 b) { 
 	if (a > b)
 		return a;
 	else
 		return b;
 }
 int main() {
-	cout << big(10, 13) << endl;
-	cout << big(14.5, 15.6);
+	cout << big(15.5, 13) << endl;
+	cout << big(10, 14.5) << endl;
+	cout << big(10, 14) << endl;
 	return 0;
 }
