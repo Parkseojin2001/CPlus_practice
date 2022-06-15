1d13eddde710a32d73b3290300c770597c963611
Parkseojin2001 <seojin10912@naver.com>
2022-06-16 02:41:59 +0900

    Upload Multiple Inheritance

--- Final exam practice/Final exam practice/소스.cpp
+++ Final exam practice/Final exam practice/소스.cpp
@@ -1,30 +1,53 @@
 #pragma warning(disable:4996)
 #include <iostream>
 using namespace std;
-class A {
-	int a, b;
+class student {
+	int id;
+	char name[20];
 public:
-	void get() {
-		cin >> a >> b;
+	void getstudent() {
+		cout << "Enter Student name: ";
+		cin >> name;
+		cout << "ID: ";
+		cin >> id;
 	}
-	int add() {
-		return a + b;
+	void putstudent() {
+		cout << "Name: " << name << endl;
+		cout << "ID: " << id << endl;
 	}
 };
-class B :public A {
-	int c, d;
+class marks {
+protected:
+	int m1, m2, m3;
 public:
-	void input() {
-		cin >> c >> d;
+	void getmarks() {
+		cout << "Enter three marks: ";
+		cin >> m1 >> m2 >> m3;
 	}
-	void sum() {
-		cout << c + d + add();
+	void putmarks() {
+		cout << "M1: " << m1 << endl;
+		cout << "M2: " << m2 << endl;
+		cout << "M3: " << m3 << endl;
+	}
+};
+class result :public student, public marks {
+	int total;
+	float average;
+public:
+	void show() {
+		total = m1 + m2 + m3;
+		average = total / 3.0;
+		cout << "Total: " << total << endl;
+		cout << "Average: " << average << endl;
 	}
 };
 int main() {
-	B obj;
-	obj.get();
-	obj.input();
-	obj.sum();
+	result obj;
+	obj.getstudent();
+	obj.getmarks();
+	obj.putstudent();
+	obj.putmarks();
+	obj.show();
 	return 0;
+
 }
