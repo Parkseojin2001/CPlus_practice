37ac1fd58f052d56513ca963d196811b794a2d10
Parkseojin2001 <seojin10912@naver.com>
2022-06-16 03:01:42 +0900

    Update Dynamic Memory Allocation
    Allocating memory using "new" (ex2)

--- Final exam practice/Final exam practice/소스.cpp
+++ Final exam practice/Final exam practice/소스.cpp
@@ -4,10 +4,8 @@ using namespace std;
 int main() {
 	int* ptr;
 	float* q;
-	ptr = new int;
-	q = new float;
-	*ptr = 10;
-	*q = 5.5;
+	ptr = new int(10);
+	q = new float(10.5);
 	cout << ptr << endl << q << endl;
 	cout << *ptr << endl << *q << endl;
 	return 0;
