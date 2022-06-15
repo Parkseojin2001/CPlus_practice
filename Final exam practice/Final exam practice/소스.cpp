5b8f182058fac05c93d3dd0b6cec2a8079b6f42e
Parkseojin2001 <seojin10912@naver.com>
2022-06-16 03:38:16 +0900

    Upload Function Overloading

--- Final exam practice/Final exam practice/소스.cpp
+++ Final exam practice/Final exam practice/소스.cpp
@@ -1,18 +1,24 @@
 #pragma warning(disable:4996)
 #include <iostream>
 using namespace std;
-class dma {
-public:
-	dma() {
-		cout << "Constructor called" << endl;
-	}
-	~dma() {
-		cout << "Destructor called" << endl;
-	}
-};
+void display(int);
+void display(float);
+void display(int, float);
 int main() {
-	dma* obj;
-	obj = new dma[4];
-	delete[]obj;
+	int a = 10;
+	float b = 20.5;
+	display(a);
+	display(b);
+	display(a, b);
 	return 0;
+}
+void display(int x) {
+	cout << "Integer number: " << x << endl;
+}
+void display(float y) {
+	cout << "Float number: " << y << endl;
+}
+void display(int a, float b) {
+	cout << "Integer number: " << a << endl;
+	cout << "float number: " << b << endl;
 }
