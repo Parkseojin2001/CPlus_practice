f5c35e95fc338ef2273858c691969e55291a7e49
Parkseojin2001 <seojin10912@naver.com>
2022-06-16 03:00:46 +0900

    Upload Dynamic Memory Allocation
    Allocating memory using "new" (ex1)

--- Final exam practice/Final exam practice/소스.cpp
+++ Final exam practice/Final exam practice/소스.cpp
@@ -1,38 +1,14 @@
 #pragma warning(disable:4996)
 #include <iostream>
 using namespace std;
-class A {
-protected:
-	int no;
-public:
-	void getno() {
-		cout << "Enter No: ";
-		cin >> no;
-	}
-};
-class B :public A {
-public:
-	void square() {
-		int sq;
-		sq = no * no;
-		cout << "square of the number: ";
-		cout << sq << endl;
-	}
-};
-class C :public A {
-public:
-	void cube() {
-		int cube;
-		cube = no * no * no;
-		cout << "Cude of number is: " << cube << endl;
-	}
-};
 int main() {
-	B bobj;
-	C cobj;
-	bobj.getno();
-	bobj.square();
-	cobj. getno();
-	cobj.cube();
+	int* ptr;
+	float* q;
+	ptr = new int;
+	q = new float;
+	*ptr = 10;
+	*q = 5.5;
+	cout << ptr << endl << q << endl;
+	cout << *ptr << endl << *q << endl;
 	return 0;
 }
