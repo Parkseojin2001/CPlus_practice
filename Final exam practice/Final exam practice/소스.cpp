d5e7151eef010d7a03ad6341952b392f4ba977c8
Parkseojin2001 <seojin10912@naver.com>
2022-06-16 03:27:33 +0900

    Upload Dynamic Object Array

--- Final exam practice/Final exam practice/소스.cpp
+++ Final exam practice/Final exam practice/소스.cpp
@@ -1,28 +1,18 @@
 #pragma warning(disable:4996)
 #include <iostream>
 using namespace std;
+class dma {
+public:
+	dma() {
+		cout << "Constructor called" << endl;
+	}
+	~dma() {
+		cout << "Destructor called" << endl;
+	}
+};
 int main() {
-	int n, i, sum = 0;
-	cout << "Enter array size: ";
-	cin >> n;
-	int* ptr;
-	ptr = new int[n];
-	if (ptr == NULL) {
-		cout << "Out of Memory";
-		return -1;
-	}
-	cout << "Memory has been created:" << endl;
-	cout << "Starting address" << ptr << endl;
-	for (i = 0; i < n; i++) {
-		cin >> *(ptr + i);
-		sum = sum + *(ptr + i);
-	}
-	cout << "****OUTPUT****" << endl;
-	cout << "Elements are:" << endl;
-	for (i = 0; i < n; i++) {
-		cout << *(ptr + i) << endl;
-	}
-	cout << "Sum: " << sum << endl;
-	delete[]ptr;
+	dma* obj;
+	obj = new dma[4];
+	delete[]obj;
 	return 0;
 }
