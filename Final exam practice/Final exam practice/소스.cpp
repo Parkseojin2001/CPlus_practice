e50015e32e71540ee00f38bc2660ee7011f2d4e7
Parkseojin2001 <seojin10912@naver.com>
2022-06-16 02:07:48 +0900

    Upload Single-level Inheritance
    Visibility Mode: public

--- Final exam practice/Final exam practice/소스.cpp
+++ Final exam practice/Final exam practice/소스.cpp
@@ -1,24 +1,39 @@
 #pragma warning(disable:4996)
 #include <iostream>
 using namespace std;
-class test1 {
-	int a, b;
+class student {
+private:
+	int id;
+	char name[20];
 public:
-	friend class test2;
-	void getab() {
-		cin >> a >> b;
+	void getstudent() {
+		cout << "Enter student name: ";
+		cin >> name;
+		cout << "Enter id: ";
+		cin >> id;
+	}
+	void putstudent() {
+		cout << "ID: " << id << endl;
+		cout << "Name: " << name << endl;
 	}
 };
-class test2 {
+class computer :public student {
+	float height, weight;
 public:
-	void putab(test1 t1) {
-		cout << t1.a << t1.b << endl;
+	void getphysical() {
+		cout << "Enter student height and weight: ";
+		cin >> height >> weight;
+	}
+	void putphysical() {
+		cout << "Height: " << height << endl;
+		cout << "Weight: " << weight << endl;
 	}
 };
 int main() {
-	test1 t1;
-	test2 t2;
-	t1.getab();
-	t2.putab(t1);
+	computer obj;
+	obj.getstudent();
+	obj.getphysical();
+	obj.putstudent();
+	obj.putphysical();
 	return 0;
 }
